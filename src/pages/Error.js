efba26277123762fe8e8cafb94b55510a0738ea9
Your Name <you@example.com>
2020-09-10 13:54:57 +0100

    Initial Commit

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,10 +1,33 @@
 import React from 'react'
-
+import { useLocation, Link } from 'react-router-dom'
+import Header from '../components/Header'
 const Error = () => {
+    const location = useLocation()
+    console.log(location);
     return (
-        <div>
-            hello from error page
-        </div>
+        <>
+            <Header />
+            <section>
+                <div className="container">
+                    <div className="section"></div>
+                    <div className="section"></div>
+                    <div className="card-panel transparent">
+                        <h3 className="center-align">
+                            ERROR 404
+                        </h3>
+                        <div className="divider"></div>
+                        <h5 className="center-align">
+                            The requested page <span className="red-text">'{location.pathname}'</span> was not found
+                        </h5>
+                        <div className="section"></div>
+                        <div className="button">
+                            <Link to='/'><p className="btn darken-3 orange hoverable">return home</p></Link>
+
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
     )
 }
 
